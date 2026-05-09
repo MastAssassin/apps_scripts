8f8eb53788289f76f21f69371cdae888f7c43f32
Lukas Riedel <lukasri271104@gmail.com>
2026-05-10 01:40:16 +0200

    added editable list

--- PathGen_Minecraft/PathGen.cpp
+++ PathGen_Minecraft/PathGen.cpp
@@ -1,10 +1,257 @@
 #include <iostream>
 #include <cstdlib>
-#include <random>
 #include "time.h"
 #include <cctype>
+#include <cstring>
+#include "errno.h"
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+
+const string f_name = "inventar.txt";
+
+class Block
+{
+  friend ostream &operator<<(ostream &outFile, const Block &a);
+  friend ostream &operator<<=(ostream &outFile, const Block &a);
+  friend class List;
+
+protected:
+  int id;
+  string name;
+  char c;
+  Block *next;
+
+public:
+  Block(string n_name, char n_c) : id(0), name(n_name), c(n_c), next(nullptr) {};
+  ~Block() {};
+
+  Block *gnext() { return next; };
+  int gid() { return id; };
+  char getc() {return c;};
+  string getname() {return name;};
+};
+
+ostream &operator<<(ostream &outFile, const Block &a)
+{
+  // outFile << "Block ID" << " || " << "      Blockname      " << " || " << "Kürzel" << endl; // 8 21 6
+  if (a.id < 10)
+    outFile << "       " << a.id << " || ";
+  if (a.id >= 10)
+    outFile << "      " << a.id << " || ";
+  int len = a.name.length();
+  int l = (21 - len) / 2;
+  int mod = (21 - len) % 2;
+  string print = a.name;
+  if (l != 0)
+  {
+    for (int i = 0; i < l; i++)
+    {
+      print = " " + print + " ";
+    }
+  }
+  if (mod != 0)
+    print = print + " ";
+
+  print = print + " || " + a.c;
+  outFile << print << endl;
+  return outFile;
+}
+
+ostream &operator<<=(ostream &outFile, const Block &a)
+{
+  outFile << a.id << ";" << a.name << ";" << a.c << endl;
+  return outFile;
+}
+class List
+{
+  friend ostream &operator<<(ostream &outFile, const List &a);
+  friend ostream &operator<<=(ostream &outFile, const List &a);
+
+private:
+  Block *head;
+  Block *tail;
+
+public:
+  List(Block *neu) : head(neu), tail(neu) {};
+  List(const List &a)
+  {
+    if (a.head == nullptr)
+    {
+      head = nullptr;
+      tail = nullptr;
+      return;
+    }
+    head = new Block(a.head->name, a.head->c);
+
+    Block *neu = head;
+    Block *alt = a.head->next;
+    while (alt != nullptr)
+    {
+      neu->next = new Block(alt->next->name, alt->next->c);
+
+      neu = neu->next;
+      alt = alt->next;
+    }
+
+    tail = neu;
+  }
+  ~List()
+  {
+
+    Block *p = head;
+
+    while (p != nullptr)
+    {
+      Block *next = p->next;
+      delete p;
+      p = next;
+    }
+  }
+  Block *gethead() {return head;};
+
+  void operator+=(Block *neu);
+  void operator-=(string name);
+  void set_default();
+  int cnt() const;
+};
+
+void List::operator+=(Block *neu)
+{
+  if (head == nullptr)
+  {
+    head = tail = neu;
+    neu->id = 0;
+    return;
+  }
+  Block *p = this->head;
+  Block *tmp = nullptr;
+
+  while (p->next != nullptr)
+  {
+    if (p->name == neu->name || p->c == neu->c)
+    {
+      cout << endl
+           << "Fehler: Name oder Kuerzel bereits vergeben." << endl;
+      return;
+    }
+    int diff = (p->next->id) - (p->id);
+    if (diff != 1)
+    {
+      tmp = p->next;
+      p->next = neu;
+      neu->next = tmp;
+      neu->id = (p->id) + 1;
+      return;
+    }
+
+    p = p->next;
+  }
+  tail->next = neu;
+  neu->id = (p->id) + 1;
+  tail = neu;
+}
+
+void List::operator-=(string name)
+{
+  if (this->head == nullptr)
+  {
+    cout << "Fehler: Liste ist leer!" << endl;
+    return;
+  }
+
+  Block *p = this->head;
+  Block *prev = nullptr;
+
+  while (p != nullptr && p->name != name)
+  {
+    prev = p;
+    p = p->next;
+  }
+
+  if (p == nullptr)
+  {
+    cout << "Fehler: Block nicht gefunden!" << endl;
+    return;
+  }
+
+  if (prev == nullptr)
+  {
+    this->head = p->next;
+
+    if (this->head == nullptr)
+    {
+      this->tail = nullptr;
+    }
+  }
+  else
+  {
+    prev->next = p->next;
+
+    if (p == this->tail)
+    {
+      this->tail = prev;
+    }
+  }
+
+  delete p;
+}
+int List::cnt() const
+{
+  Block *p = head;
+  int i = 0;
+  while (p != nullptr)
+  {
+    p = p->next;
+    i++;
+  }
+  return i;
+}
+
+ostream &operator<<(ostream &outFile, const List &a)
+{
+  if (a.head == nullptr)
+  {
+    outFile << "Fehler: Liste ist leer!" << endl;
+    return outFile;
+  }
+  Block *p = a.head;
+  outFile << "Anzahl gespeicherter Blöcke: " << a.cnt() << endl
+          << "-------------------------------" << endl
+          << "Block ID" << " || " << "      Blockname      " << " || " << "Kuerzel" << endl
+          << "--------------------------------------------" << endl;
+  while (p != nullptr)
+  {
+    outFile << *p;
+    p = p->gnext();
+  }
+  return outFile;
+}
+
+ostream &operator<<=(ostream &outFile, const List &a)
+{
+  Block *p = a.head;
+  while (p != nullptr)
+  {
+    outFile <<= *p;
+    p = p->gnext();
+  }
+  outFile << endl;
+  return outFile;
+}
+
+void List::set_default()
+{
+  string sarr[5] = {"stone", "stonebrick", "cobblestone", "(chiseled) dirt", "andesite"};
+  char carr[5] = {'s', 'b', 'c', 'd', 'a'};
+
+  for (int i = 0; i < 5; i++)
+  {
+    Block *neu = new Block(sarr[i], carr[i]);
+    (*this) += neu;
+  }
+}
 
 class Path
 {
@@ -24,67 +271,12 @@ public:
     delete next;
   }
 
-  char *set_arr();
-  void chain(int len, int wide);
+  char *set_arr(List *inventory);
+  void chain(int len, int wide, List *inventory);
 };
-
-char *Path::set_arr()
-{
-  path = new char[path_wide];
-  for (int i = 0; i <= (path_wide); i++)
-  {
-    int n = rand() % 6;
-    char c;
-    switch (n)
-    {
-    case 0:
-      c = 'g';
-      break;
-    case 1:
-      c = 's';
-      break;
-    case 2:
-      c = 'b';
-      break;
-    case 3:
-      c = 'c';
-      break;
-    case 4:
-      c = 'd';
-      break;
-    case 5:
-      c = 'a';
-      break;
-    default:
-      c = '?';
-      break;
-    }
-
-    path[i] = c;
-  }
-  return path;
-}
-
-void Path::chain(int len, int wide)
-{
-  Path *p = this;
-  for (int i = 1; i < len; ++i)
-  {
-    p->next = new Path(len - i, wide);
-    p = p->next;
-    p->set_arr();
-  }
-}
 
 ostream &operator<<(ostream &outFile, const Path &a)
 {
-  outFile << "g = gravel\n"
-          << "s = stone\n"
-          << "b = stonebrick\n"
-          << "c = cobblestone\n"
-          << "d = (chiseled) dirt\n"
-          << "a = andesite\n"
-          << endl;
   int z = 0;
   const Path *current = &a;
   while (current != nullptr)
@@ -102,35 +294,284 @@ ostream &operator<<(ostream &outFile, const Path &a)
   return outFile;
 }
 
+
+char *Path::set_arr(List *inventory)
+{
+  path = new char[path_wide];
+  for (int i = 0; i <= (path_wide); i++)
+  {
+    int n = rand() % inventory->cnt();
+
+    Block *p = inventory->gethead();
+    for (int z = 0; z < n; z++) 
+    {p = p ->gnext(); }
+
+    path[i] = (p->getc());
+  }
+  return path;
+}
+
+void Path::chain(int len, int wide, List *inventory)
+{
+  Path *p = this;
+  for (int i = 1; i < len; ++i)
+  {
+    p->next = new Path(len - i, wide);
+    p = p->next;
+    p->set_arr(inventory);
+  }
+}
+
+void gen(List *iventory);
+void add(List *inventory);
+void ls(List *inventory);
+void del(List *inventory);
+List *read();
+void write(List *inventory);
+
 int main(int argc, const char **argv)
 {
   srand(time(nullptr));
+  bool changed = false;
 
-  if (argc != 3)
+  List *inventory = read();
+  if (inventory->cnt() == 0)
   {
-    cout << "Syntaxfehler: PathGen.exe <Laenge des Pfades> <Breite des Pfades>" << endl;
-    return 1;
+    Block *gravel = new Block("gravel", 'g');
+    *inventory += gravel;
+    inventory->set_default();
+    cin.clear();
+    changed = true;
   }
-  int len, wide;
+  string word;
 
-  if (isdigit(*argv[1]) == false || isdigit(*argv[2]) == false)
+  if (argc != 2)
   {
-    cout << "Fehler: Argumente muessen ganze Zahlen sein." << endl;
-    return 1;
+    cout << "Funktionen:" << endl
+         << "Pfad generieren             : " << "gen" << endl
+         << "Blockbibliothek erweitern   : " << "add" << endl
+         << "Blockbibliothek verkleinern : " << "del" << endl
+         << "Blockbibliothek ausgeben    : " << "ls" << endl
+         << "Prgramm beenden             : " << "end" << endl;
+    cout << endl
+         << "Wenn mir jemals langweilig werden sollte baue ich vllt eine grafische Oberflaeche." << endl;
+    cout << "Warte auf Eingabe: ";
+    cin >> word;
+    cin.clear();
+    cout << endl;
   }
-  len = atoi(argv[1]);
-  wide = atoi(argv[2]);
+  else
+  {
+    word = argv[1];
+  }
+
+  for (;;)
+  {
+    if (word == "gen") // tatsächlich können switch cases keine strings, nur ints und chars
+    {
+      gen(inventory);
+    }
+    else if (word == "add")
+    {
+      add(inventory);
+      changed = true;
+    }
+    else if (word == "del")
+    {
+      changed = true;
+      del(inventory);
+    }
+    else if (word == "ls")
+    {
+      ls(inventory);
+    }
+    else if (word == "end")
+    {
+      if (changed == true)
+        write(inventory);
+      delete inventory;
+      exit(EXIT_SUCCESS);
+    }
+    else
+    {
+      cout << "Fehler, falsche Eingabe!" << endl
+           << endl;
+    }
+
+    cout << "Funktionen:" << endl
+         << "Pfad generieren             : " << "gen" << endl
+         << "Blockbibliothek erweitern   : " << "add" << endl
+         << "Blockbibliothek verkleinern : " << "del" << endl
+         << "Blockbibliothek ausgeben    : " << "ls" << endl
+         << "Prgramm beenden             : " << "end" << endl
+         << "Warte auf Eingabe           : ";
+    cin >> word;
+    cout << endl;
+  }
+}
+
+void gen(List *inventory)
+{
+  int len, wide = 0;
+  cout << "Warte auf Eingabe, Laenge: ";
+  cin >> len;
+  if (cin.fail())
+  {
+    cout << endl
+         << "Fehler: Eingabe muss numerisch sein\n\n";
+    cin.clear();
+    return;
+  }
+  cout << "Warte auf Eingabe, Breite: ";
+  cin >> wide;
+  if (cin.fail())
+  {
+    cout << endl
+         << "Fehler: Eingabe muss numerisch sein\n\n";
+    return;
+  }
+
   if (len <= 0 || wide <= 0)
   {
-    cout << "Fehler: Argumente muessen groesser als 0 sein" << endl;
-    return 1;
+
+    cout << endl
+         << "Fehler: Werte > 0 erforderlich\n\n";
+    return;
   }
 
+  if (wide > 60)
+  {
+    cout << endl
+         << "Jetzt uebertreibst du es aber." << endl
+         << endl;
+    return;
+  }
+  if (wide > 50)
+  {
+    cout << endl
+         << "Das ist kein(e) Strasse/Weg mehr, das ist ein Platz!" << endl;
+  }
+  Block *p = inventory->gethead();
+  cout << endl<< "Kuerzel" << " || " << "Blockname" << endl;
+  cout << "--------------------------------" << endl;
+  for (int i = 0; i < inventory->cnt(); i++)
+  {
+    cout << "   " << p->getc() << "   " << " || " << p->getname() << endl; 
+    p = p ->gnext();
+  }
+  cout << endl; 
+
   Path *head = new Path(len, wide);
-  head->set_arr();
-  head->chain(len, wide);
+  head->set_arr(inventory);
+  head->chain(len, wide, inventory);
 
   cout << *head;
   delete head;
-  return 0;
+  return;
+}
+
+void add(List *inventory)
+{
+  string name = " ";
+  char c = ' ';
+  do
+  {
+    cout << "Erwarte Eingabe, Blockname: ";
+    cin >> name;
+    if (name.length() > 21)
+    {
+      cout << endl
+           << "Fehler: Name darf maximal 21 Zeichen lang sein.\n\n";
+      cin.clear();
+      name = " ";
+    }
+  } while (name == " ");
+
+  do
+  {
+    cout << "Erwarte Eingabe, Blockkuerzel: ";
+    cin >> c;
+  } while (c == ' ');
+
+  Block *neu = new Block(name, c);
+  // cout << neu;
+  (*inventory) += neu;
+  cin.clear();
+}
+
+void ls(List *inventory)
+{
+  cout << *inventory << endl;
+}
+
+void del(List *inventory)
+{
+  ls(inventory);
+
+  cout << endl
+       << "Welcher Block soll entfert werden?" << endl;
+  string name = " ";
+  do
+  {
+    cout << "Erwarte Eingabe, Blockname: ";
+    cin >> name;
+    if (name.length() > 21)
+    {
+      cout << endl
+           << "Fehler: Name darf maximal 21 Zeichen lang sein.\n\n";
+      cin.clear();
+      name = " ";
+    }
+  } while (name == " ");
+
+  (*inventory) -= name;
+}
+
+List *read()
+{
+  List *inventory = new List(nullptr);
+  ifstream file(f_name);
+
+  if (!file.is_open())
+  {
+    cout << "Fehler: Blockbibliothek konnte nicht geoeffnet werden." << endl
+         << "Lege neue Bibliothek an." << endl;
+    //   cout << "lebe noch" << endl;
+    return inventory;
+  }
+  string line;
+  while (getline(file, line))
+  {
+    stringstream ss(line);
+    string name;
+    string id;
+    string kr;
+
+    getline(ss, id, ';');
+    getline(ss, name, ';');
+    getline(ss, kr, '\n');
+
+    char c = kr[0];
+    Block *neu = new Block(name, c);
+    (*inventory) += neu;
+  }
+  file.close();
+  return inventory;
+}
+
+void write(List *inventory)
+{
+  ofstream file(f_name);
+
+  if (!file.is_open())
+  {
+    std::cerr << "Fehler beim Erstellen der Datei\n";
+    std::cerr << "errno: " << errno << "\n";
+    std::cerr << "Beschreibung: "
+              << std::strerror(errno) << "\n";
+    exit(EXIT_FAILURE);
+  }
+  file <<= *inventory;
+  cout << "Bibliothek wurde erfolreich gespeichert!";
+  file.close();
 }
