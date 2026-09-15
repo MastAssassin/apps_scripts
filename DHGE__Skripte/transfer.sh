b09d082b46a7e69a98304d98d9639c8386a535d7
MastAssassin <Lukas.Riedel27@proton.me>
2026-09-15 12:40:25 +0200

    Fix typo in comments of transfer.sh

--- DHGE__Skripte/transfer.sh
+++ DHGE__Skripte/transfer.sh
@@ -4,4 +4,4 @@ sudo mount -t cifs //192.168.71.100/Transfer /mnt/share -o username=<username>,u
 
 #replace <username> with your dhge-name  e.g.  mustma.pi24 
 #I don't know if the name is alreade given  must --> Mustermann ma --> Max
-#if you lower the ids you maybe won't be able write and execute, so just set your name and everthing will work 
+#if you lower the ids you maybe won't be able to write and execute, so just set your name and everthing will work 
